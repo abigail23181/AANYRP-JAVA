ef81df55cfb2b8385f8f04232ecb27cfb6743249
abigail23181 <abigailrodrigxz@gmail.com>
2026-05-19 14:44:50 -0600

    Nuria envio Ejercicio6

---
+++ EJERCICIOS/Ejercicio6.java
@@ -0,0 +1,7 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + " x 5 = " + (i * 5));
+        }
+    }
+}
